639d4c818a201cf111de9cfceaa5e50ee43c63f0
TopMyster <topmyster@outlook.com>
2025-10-31 23:43:33 -0500

    Adds all the basic styles and functions

---
+++ script.js
@@ -0,0 +1,28 @@
+let user = document.getElementById('usertext')
+let text = ['hu', 'hola']
+let resultEl = document.getElementById('resulttext')
+let int = 0
+let slider = document.getElementById('speed')
+let speed
+let preview = document.getElementById('speedpreview')
+
+setInterval(() => {
+     slider.addEventListener('input', () => {
+        speed = slider.value
+        preview.textContent = `${Math.floor(60000/speed)} WPM`
+
+    })
+},0)
+
+function Play() {
+  int = 0
+  let timer = setInterval(() => {
+    resultEl.textContent = text[int]
+    int++
+    if (int >= text.length) clearInterval(timer)
+  }, speed)
+}
+
+function Clear() {
+    user.value = ''
+}
